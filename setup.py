943290344fb6b281f6bc279a8a94adadad7c703c
Andrew Colin Kissa <andrew@topdog.za.net>
2013-04-21 11:36:30 +0200

    * Release version 2.0.1

--- setup.py
+++ setup.py
@@ -68,7 +68,7 @@ def read(fname):
 
 setup(
     name='baruwa',
-    version='2.0.0',
+    version='2.0.1',
     description='Ajax enabled MailScanner web frontend',
     long_description=read('README.rst'),
     author='Andrew Colin Kissa',
