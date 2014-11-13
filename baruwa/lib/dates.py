1b2d96f6126b8d88cbe76fc2665ffc43d6cbfc0b
Andrew Colin Kissa <andrew@topdog.za.net>
2014-11-13 18:18:52 +0200

    * FIX: Regression introduced update, #53

--- baruwa/lib/dates.py
+++ baruwa/lib/dates.py
@@ -1,21 +1,22 @@
 # -*- coding: utf-8 -*-
 # vim: ai ts=4 sts=4 et sw=4
 # Baruwa - Web 2.0 MailScanner front-end.
-# Copyright (C) 2010-2012  Andrew Colin Kissa <andrew@topdog.za.net>
+# Copyright (C) 2010-2014  Andrew Colin Kissa <andrew@topdog.za.net>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
 # the Free Software Foundation, either version 3 of the License, or
 # (at your option) any later version.
-# 
+#
 # This program is distributed in the hope that it will be useful,
 # but WITHOUT ANY WARRANTY; without even the implied warranty of
 # MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 # GNU General Public License for more details.
-# 
+#
 # You should have received a copy of the GNU General Public License
 # along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #
+"Date manipulation functions"
 
 import pytz
 
@@ -35,6 +36,38 @@ def now():
 
 def convert_date(date, tmzone):
     "Convert UTC date to timezone"
-    if isinstance(tmzone, basestring):
-        tmzone = pytz.timezone(tmzone)
+    tmzone = make_tz(tmzone)
     return pytz.UTC.normalize(date).astimezone(tmzone)
+
+
+def make_tz(tzinfo):
+    "Convert timezone string to timezone"
+    if isinstance(tzinfo, basestring):
+        tzinfo = pytz.timezone(tzinfo)
+    return tzinfo
+
+
+def startday():
+    "Return start time"
+    date = now()
+    return date.replace(hour=00, minute=00, second=00)
+
+
+def endday():
+    "Return end time"
+    date = now()
+    return date.replace(hour=23, minute=59, second=59)
+
+
+def ustartday(tmz):
+    "Get users localized start day in UTC"
+    currentdate = datetime.now(make_tz(tmz))
+    currentdate = currentdate.replace(hour=00, minute=00, second=00)
+    return pytz.utc.normalize(currentdate.astimezone(pytz.utc))
+
+
+def uendday(tmz):
+    "Get users localized end day in UTC"
+    currentdate = datetime.now(make_tz(tmz))
+    currentdate = currentdate.replace(hour=23, minute=59, second=59)
+    return pytz.utc.normalize(currentdate.astimezone(pytz.utc))
